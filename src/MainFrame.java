ffa6d12e178b2d41e017b9212df331d9275decdf
qwe329295 <105021028@live.asia.edu.tw>
2017-10-02 23:56:10 +0800

    commit
    hw20171002

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,21 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 public class MainFrame extends JFrame{
     private JLabel lab1 = new JLabel("Hit:0");
-    private JLabel lab2 = new JLabel();
-    private JLabel lab3 = new JLabel();
-    private JLabel lab4 = new JLabel();
-    private JLabel lab5 = new JLabel();
+    private JLabel labgun = new JLabel();
+    private JLabel labbullet = new JLabel();
+    private JLabel labtarget = new JLabel();
     private JButton btnAuto =new JButton("Auto move");
-    private JButton btnleft=new JButton("<-");
+    private JButton btnleft=new JButton("往左飄移");
     private JButton btnfire =new JButton("fire");
-    private JButton btnright =new JButton("->");
+    private JButton btnright =new JButton("往右飄移");
     private JButton btnexit =new JButton("exit");
-    private JPanel jpan1=new JPanel(new GridLayout(1,1,1,1));
-    private JPanel jpan2=new JPanel(new GridLayout(2,5,1,1));
+    private Timer t1fire;
+    private Timer t2;
+    private int labX=250,labY=350;
+    private int bulletX,bulletY,count=0;
+    private Random rdm = new Random();
             ImageIcon img=new ImageIcon("sun.jpg");
-            ImageIcon imgbullet=new ImageIcon("sun.jpg");
-            ImageIcon imgtriangle=new ImageIcon("sun.jpg");
+            ImageIcon imgbullet=new ImageIcon("bullet.jpg");
+            ImageIcon imggun=new ImageIcon("gun.jpg");
+    private boolean flag=(true);
 
             private Container cp;
     public  MainFrame(){
@@ -23,25 +30,118 @@ public class MainFrame extends JFrame{
     }
     public void init(){
         cp =this.getContentPane();
-        this.setBounds(150,200,600,400);
+        this.setBounds(150,200,700,600);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        cp.setLayout( new BorderLayout(5,5));
-        cp.add(jpan1,BorderLayout.CENTER);
-        cp.add(jpan2,BorderLayout.SOUTH);
+        this.setResizable(false);
+        cp.setLayout( null);
+        cp.add(labgun);
+        labgun.setIcon(imggun);
+        labgun.setBounds(labX,labY,100,66);
+
+        cp.add(labbullet);
+        labbullet.setIcon(imgbullet);
+        labbullet.setBounds(1000,100,100,100);
+        labbullet.setVisible(false);
+        cp.add(labtarget);
+        labtarget.setIcon(img);
+        labtarget.setBounds(rdm.nextInt(600)+1,50,100,50);
 
 
-        jpan2.add(lab1);
-        jpan2.add(btnleft);
-        jpan2.add(btnfire);
-        jpan2.add(btnright);
-        jpan2.add(lab2);
-        jpan2.add(btnAuto);
-        jpan2.add(lab3);
-        jpan2.add(lab4);
-        jpan2.add(lab5);
-        jpan2.add(btnexit);
+        cp.add(lab1);
+        lab1.setBounds(10,450,80,50);
+        cp.add(btnAuto);
+        btnAuto.setBounds(10,500,100,50);
+        cp.add(btnleft);
+        btnleft.setBounds(220,500,100,50);
+        cp.add(btnfire);
+        btnfire.setBounds(340,500,100,50);
+        btnfire.setBackground(new Color(255, 52, 3));
+        btnfire.setForeground(Color.WHITE);
+        cp.add(btnright);
+        btnright.setBounds(460,500,100,50);
+        cp.add(btnexit);
+        btnexit.setBounds(580,500,100,50);
 
 
+        btnexit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
+
+        btnleft.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               labX-=10;
+               labgun.setLocation(labX,350);
+               t2.stop();
+            }
+        });
+        btnfire.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labbullet.setVisible(true);
+                bulletX=labgun.getX();
+                bulletY=labgun.getY();
+                t1fire.start();
+            }
+        });
+
+
+        btnright.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               labX+=10;
+               labgun.setLocation(labX,350);
+               t2.stop();
+            }
+        });
+        btnAuto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               t2.start();
+            }
+        });
+
+
+        t1fire =new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bulletY-=10;
+                labbullet.setLocation(bulletX,bulletY);
+                if(bulletX<labtarget.getX()+labtarget.getWidth()&&bulletX>labtarget.getX()&&bulletY==50){
+                    count++;
+                    lab1.setText("Hit:"+count);
+                    labtarget.setLocation(rdm.nextInt(600)+1,50);
+                }
+                if(bulletY <= 0){
+                    labbullet.setVisible(false);
+                    labbullet.setLocation(bulletX,bulletY);
+                    labY=labgun.getY();
+                    t1fire.stop();
+                }
+            }
+
+        });
+        t2 =new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(flag){
+                    labX+=10;
+                    labgun.setLocation(labX,350);
+                    if(labX>MainFrame.this.getWidth()-120){
+                        flag=false;
+                    }
+                }else if(!flag){
+                    labX-=10;
+                    labgun.setLocation(labX,350);
+                    if(labX<0){
+                        flag=true;
+                    }
+                }
+            }
+        });
     }
 }
